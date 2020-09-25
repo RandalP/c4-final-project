3da4412004f8c192a17f152158a7686b499cf98c
Randal Parsons <Randal.Parsons@lucidogroup.io>
2020-09-25 17:30:04 +1000

    Add initial version of createTodos

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,12 +1,47 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import * as AWS from 'aws-sdk'
+import * as uuid from 'uuid'
+import middy from 'middy'
+import { cors } from 'middy/middlewares'
+import { getUserId } from '../utils'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todoTable = process.env.TODO_TABLE
+
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log('Caller event', event)
+
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
+  const userId = getUserId(event)
+  const todoId = uuid.v4()
+  const createdAt = new Date().toISOString()
 
-  // TODO: Implement creating a new TODO item
-  return undefined
-}
+  const newItem = {
+    userId,
+    todoId,
+    createdAt,
+    ...newTodo
+  }
+  console.log('Storing new item: ', newItem)
+
+  await docClient.put({
+    TableName: todoTable,
+    Item: newItem
+  }).promise()
+  return {
+    statusCode: 201,
+    body: JSON.stringify({
+      item: newItem
+    })
+  }
+})
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
