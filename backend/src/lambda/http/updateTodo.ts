d96c4b8bc4d3aba2d4b3441beef63026f11952e6
Randal Parsons <Randal.Parsons@lucidogroup.io>
2020-09-25 17:31:31 +1000

    Initial version of updateTodo

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,13 +1,58 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import * as AWS from 'aws-sdk'
+import middy from 'middy'
+import { cors } from 'middy/middlewares'
+import { getUserId } from '../utils'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todoTable = process.env.TODO_TABLE
+
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log('Caller event', event)
+
+  // Update a TODO item with the provided id using values in the "updatedTodo" object
+  const userId = getUserId(event)
   const todoId = event.pathParameters.todoId
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
 
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
-}
+  docClient.update(
+    {
+      TableName: todoTable,
+      Key: {
+        userId,
+        todoId
+      },
+      UpdateExpression: "set #name = :name, dueDate = :dueDate, done = :done",
+      ExpressionAttributeValues: {
+        ":name": updatedTodo.name,
+        ":dueDate": updatedTodo.dueDate,
+        ":done": updatedTodo.done
+      },
+      ExpressionAttributeNames: {
+        "#name": "name"
+      },
+      ReturnValues: "UPDATED_NEW"
+    },
+    function (err, data) {
+      if (err) {
+        console.error("Unable to update TODO. Error JSON:", JSON.stringify(err, null, 2));
+      } else {
+        console.log("Update TODO succeeded:", JSON.stringify(data, null, 2));
+      }
+    }
+  )
+  return {
+    statusCode: 204,
+    body: undefined
+  }
+})
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
