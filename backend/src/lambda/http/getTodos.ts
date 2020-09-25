003ce544b4fdbc5cd7f2ddb40f63e1b24bd0c6de
Randal Parsons <Randal.Parsons@lucidogroup.io>
2020-09-25 17:29:17 +1000

    Add initial version of getTodos

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,43 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as AWS from 'aws-sdk'
+import middy from 'middy'
+import { cors } from 'middy/middlewares'
+import { getUserId } from '../utils'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todoTable = process.env.TODO_TABLE
+
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log('Caller event', event)
+
+  const userId = getUserId(event)
+  const todos = await getTodos(userId)
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify({
+      items: todos
+    })
+  }
+})
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
+
+async function getTodos(userId: string) {
+  const result = await docClient.query({
+    TableName: todoTable,
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+      ':userId': userId
+    },
+    ScanIndexForward: false
+  }).promise()
+
+  return result.Items
 }
